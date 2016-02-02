e2c0e1f1d5b6907b3824629c647fb9077d6775b2
Spencer <scarvill91@gmail.com>
2016-02-02 14:04:59 -0600

    Change destination directories for distributables

--- gen_dists.sh
+++ gen_dists.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 {
   cd "${BASH_SOURCE%/*}" || exit
-  mkdir -p dist/js dist/css
+  mkdir -p dist/javascripts dist/stylesheets
 
   jade views/index.jade --out dist/
 
   coffee -c lib/*.coffee
-  minify lib/tic_tac_toe_api.js lib/game.js lib/game_prompt.js lib/game_view.js lib/main.js --output dist/js/tttapp.js
-  sed -i.bak "1,5d" dist/js/tttapp.js
-  rm lib/*.js dist/js/*.bak
+  minify lib/tic_tac_toe_api.js lib/game.js lib/game_prompt.js lib/game_view.js lib/main.js --output dist/javascripts/tttapp.js
+  sed -i.bak "1,5d" dist/javascripts/tttapp.js
+  rm lib/*.js dist/javascripts/*.bak
 
-  minify public/stylesheets/style.css --output dist/css/style.css
+  minify public/stylesheets/style.css --output dist/stylesheets/style.css
 } > /dev/null
